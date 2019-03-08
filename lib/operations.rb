9ba6ba68bb94ae48dbd61fbd6854dd2e7dd28f63
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-08 20:04:19 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+if speed > 40 || speed < 60 == true #right idea, but you had it in the wrong place
 end
+else
+	false
+end
+
 
 
 
 def not_safe?(speed)
-	
+  not_safe ? "speed < 40" : "speed > 60"
+
 end
-	
-
-
